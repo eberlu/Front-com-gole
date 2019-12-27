d218ddd41067ef9a8bdec7c572a9e6dae7cae86a
Luciano <lucianochitao1995@hotmail.com>
2019-12-27 16:15:19 -0300

    assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,35 @@
+const env = 'dev'
+
 const 
 {src, dest, series,watch} = require('gulp')
 ,clean = require('gulp-clean')
 ,twig = require('gulp-twig')
+,named = require('vinyl-named')
+,sass = require('gulp-sass')
+,uglify = require('gulp-uglify-es').default
+,webpackStream = require('webpack-stream')
 
 const configs = {
     dist: 'dist',
     src: 'src',
     templates:{
         ext: '.twig'
+    },
+    scss:{
+        files: [ 'src/scss/main.scss' ]
+    },
+    javascripts:{
+        files: ['src/js/layout.js']
     }
 }
 
 function Clean(){
-    return src(configs.dist).pipe(clean({read:false}));
+    return src(configs.dist).pipe(clean({read:false, allowEmpty: false}));
 }
 
 function Templates(){
     return new Promise((resolve,reject) => {
-        src(`${configs.src}/[^_]*${configs.templates.ext}`)
+        src(`${configs.src}/views/[^_]*${configs.templates.ext}`)
         .pipe(twig())
         .pipe(dest(configs.dist))
         .on('error', reject)
@@ -25,5 +37,32 @@ function Templates(){
     })
 }
 
-exports.clean = Clean;
-exports.templates = Templates;
+function Sass(){
+    return new Promise((resolve,reject)=>{
+        src(configs.scss.files)
+        .pipe(sass({sourceComments:false}).on('error', sass.logError))
+        .pipe(dest(`${configs.dist}/css`))
+        .on('error', reject)
+        .on('end', resolve)
+    })
+}
+
+function Javascripts(){
+    return new Promise((resolve,reject)=>{
+        let fileStream = src(configs.javascripts.files)
+        .pipe(named())
+        .pipe(webpackStream())
+
+        if(env == 'prod') fileStream = fileStream.pipe(uglify())
+        
+        fileStream.pipe(dest('dist/js'))
+        .on('error', reject)
+        .on('end', resolve)
+    })
+}
+
+exports.clean = Clean
+exports.templates = Templates
+exports.sass = Sass
+exports.js = Javascripts
+exports.default = series(Clean, Templates, Sass, Javascripts)
---
+++ src/js/layout.js
@@ -0,0 +1,5 @@
+
+const $ = require('jquery')
+
+//plugins globais
+window.$ = $;
