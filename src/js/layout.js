beaad04a3dd548a79666d1e4a60bfcad28b5a0a9
Luciano <lucianochitao1995@hotmail.com>
2020-01-15 21:33:00 -0300

    codigo bonito

--- src/js/layout.js
+++ src/js/layout.js
@@ -8,14 +8,6 @@ const swiper = require('swiper')
 window.$ = $;
 window.swiper = swiper;
 
-//dropdowns criados by joy integrados com os do bts4 
-$('.dropdown.dropdown-scale-wrap').on('show.bs.dropdown', function(e){
-    $(this).children('.dropdown-scale').addClass('active');
-})
-$('.dropdown.dropdown-scale-wrap').on('hidden.bs.dropdown', function(e){
-    $(this).children('.dropdown-scale').removeClass('active');
-})
-
 new swiper.default('.menu-slider',{
     slidesPerView: 1,
 });
